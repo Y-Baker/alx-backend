4df2da0b2e718d5ac79ffa7dbf3469a73712f7fd
Yousef <yuossefbakier@gmail.com>
2024-03-15 17:50:26 +0200

    Add Task 5 IN i18n

--- 0x02-i18n/config_babel.py
+++ 0x02-i18n/config_babel.py
@@ -6,3 +6,10 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
