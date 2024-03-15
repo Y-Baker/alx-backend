30e28155cf0aba9eede6d0161de871f85443ef62
Yousef <yuossefbakier@gmail.com>
2024-03-15 16:32:55 +0200

    Add Task 3 IN i18n

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+
+from flask import Flask, render_template, request
+from config_babel import Config
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def get_locale():
+    """ Get locale """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def home():
+    """ Home page """
+    return render_template('3-index.html')
