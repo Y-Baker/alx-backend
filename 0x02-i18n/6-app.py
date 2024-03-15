696c869fe30683072ddc34a80d7e8b5952a9284b
Yousef <yuossefbakier@gmail.com>
2024-03-15 17:58:20 +0200

    Add Task 6 IN i18n

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+
+from flask import Flask, render_template, request, g
+from config_babel import Config
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def get_locale():
+    """ Get locale """
+    langs = request.args.get('locale', '')[1:-1].split('|')
+    for lang in langs:
+        if lang in app.config['LANGUAGES']:
+            return lang
+    user = g.user
+    if user and user.locale in app.config['LANGUAGES']:
+        return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """ Get user """
+    from config_babel import users
+    id = request.args.get('login_as', None)
+    if id:
+        id = int(id)
+        return users.get(id, None)
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Before request """
+    from flask import g
+    g.user = get_user()
+
+
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def home():
+    """ Home page """
+    return render_template('6-index.html')
