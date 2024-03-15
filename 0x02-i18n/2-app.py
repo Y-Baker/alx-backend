ef3a202a1095db56d10167b56e7cbff7d25d8aee
Yousef <yuossefbakier@gmail.com>
2024-03-15 16:11:33 +0200

    Add Task 2 IN i18n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
 
+from flask import Flask, render_template, request
 from config_babel import Config
-from flask import Flask, render_template
 from flask_babel import Babel
-from flask import request
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+def get_locale():
+    """ Get locale """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def home():
     """ Home page """
     return render_template('2-index.html')
-
-
-@babel.localeselector
-def get_locale():
-    """ Get locale """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
